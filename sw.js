22cd8dfaab81251f375fed5d62871a03d6496ed0
xentury130720 <95573342+xentury130720@users.noreply.github.com>
2026-08-13 11:49:31 +0700

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,21 @@
+var CACHE='poaf-lpp-v1';
+var FILES=['./','./index.html','./manifest.json','./icon-180.png','./icon-192.png','./icon-512.png'];
+self.addEventListener('install',function(e){
+  self.skipWaiting();
+  e.waitUntil(caches.open(CACHE).then(function(c){return c.addAll(FILES);}).catch(function(){}));
+});
+self.addEventListener('activate',function(e){
+  e.waitUntil(caches.keys().then(function(ks){
+    return Promise.all(ks.map(function(k){return k===CACHE?null:caches.delete(k);}));
+  }).then(function(){return self.clients.claim();}));
+});
+self.addEventListener('fetch',function(e){
+  if(e.request.method!=='GET')return;
+  e.respondWith(caches.match(e.request).then(function(r){
+    return r || fetch(e.request).then(function(resp){
+      var cp=resp.clone();
+      caches.open(CACHE).then(function(c){c.put(e.request,cp);}).catch(function(){});
+      return resp;
+    }).catch(function(){return caches.match('./index.html');});
+  }));
+});
