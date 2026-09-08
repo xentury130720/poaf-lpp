7e1f8ddccf6ca30576e60219237db09768d7b1ce
xentury130720 <95573342+xentury130720@users.noreply.github.com>
2026-09-09 01:07:50 +0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-var CACHE='poaf-lpp-v14';
+var CACHE='poaf-lpp-v15';
 var FILES=['./','./index.html','./manifest.json','./icon-180.png','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',function(e){
   self.skipWaiting();
